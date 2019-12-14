e13f6dd0c19a6c5a1cf30c171f617636780d9418
Werenskjold <Rilliden@gmail.com>
2019-12-14 22:34:50 +0900

    Initial Commit
    Movement Code Completed

---
+++ Ball Bopper/Assets/Scripts/GameplayControllerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayControllerScript : MonoBehaviour
+{
+    #region Helper Functions
+    public Vector3 RetrieveMousePosition()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        return mousePosition;
+    }
+    #endregion
+
+    /// Pre-assigned Variables
+    [SerializeField]
+    private GameObject ballGameObject = null;
+
+    /// Public Variables
+    public float maxPushForce = 600.0f;
+
+    /// Private Variables
+    private float falloffDistance = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 touchPosition = RetrieveMousePosition();
+            Debug.Log("Touch occured at: <" + touchPosition.x + ", " + touchPosition.y + ">");
+
+            Vector2 forceDirection = -1 * (touchPosition - ballGameObject.GetComponent<Transform>().position);
+            float distance = forceDirection.magnitude;
+            forceDirection.Normalize();
+
+            float forceStrengthMultiplier = Mathf.Exp(-(distance / falloffDistance));
+
+            ballGameObject.GetComponent<Rigidbody2D>().AddForce(forceDirection * forceStrengthMultiplier * maxPushForce);
+
+            Debug.Log("Force of " + forceStrengthMultiplier * maxPushForce + " applied");
+        }
+    }
+}
