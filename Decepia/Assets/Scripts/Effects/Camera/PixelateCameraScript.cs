a92f859d593578e935bc7a94482ddc99b78d0b3f
Werenskjold <Rilliden@gmail.com>
2020-01-25 20:06:35 +0900

    Added Decepia

---
+++ Decepia/Assets/Scripts/Effects/Camera/PixelateCameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelateCameraScript : MonoBehaviour
+{
+    public Material pixelateMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, pixelateMaterial);
+    }
+}
---
+++ Decepia/Assets/Scripts/Environment/SkyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyScript : MonoBehaviour
+{
+    public Transform playerPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, transform.position.y, playerPosition.position.z);
+    }
+}
