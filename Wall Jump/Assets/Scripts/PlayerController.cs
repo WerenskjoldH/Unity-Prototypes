f5a08a8332739e9b469c62fa0cf06986582d7983
Werenskjold <Rilliden@gmail.com>
2020-02-09 21:43:11 +0900

    Player Movement: Launch Line

--- Wall Jump/Assets/Scripts/PlayerController.cs
+++ Wall Jump/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float launchPower = 100.0f;
+    // Both Launch Properties Use World Units
+    public float launchPower = 50.0f;
+    public float maxLaunchMult = 3.0f;
     public JumpCounterScript jumpCounterScript;
     public int totalJumpsAllowed = 3;
 
@@ -32,17 +34,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
 
         if (totalJumpsMade < totalJumpsAllowed)
         {
             if (Input.GetMouseButton(0))
             {
                 stuckToSurface = false;
-                
+
+                Vector3 powerLine = (mousePos - transform.position);
+                float powerLineMagnitude = Mathf.Min(powerLine.magnitude, maxLaunchMult);
+                powerLine.Normalize();
+
+
                 LineRenderer lr = gameObject.GetComponent<LineRenderer>();
                 lr.enabled = true;
                 lr.SetPosition(0, transform.position);
-                lr.SetPosition(1, mousePos);
+                lr.SetPosition(1, transform.position + powerLineMagnitude * powerLine);
             }
 
             if (Input.GetMouseButtonUp(0))
@@ -51,8 +59,10 @@ public class PlayerController : MonoBehaviour
                 jumpCounterScript.ReduceJumps();
                 gameObject.GetComponent<LineRenderer>().enabled = false;
                 gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-                Vector2 diffVec = (mousePos - transform.position);
-                gameObject.GetComponent<Rigidbody2D>().AddForce(launchPower * diffVec);
+                Vector2 differenceVector = (mousePos - transform.position);
+                Vector2 launchForce = launchPower * Mathf.Min(differenceVector.magnitude, maxLaunchMult) * differenceVector.normalized;
+                Debug.Log(launchForce.magnitude);
+                gameObject.GetComponent<Rigidbody2D>().AddForce(launchForce);
             }
         }
     }
