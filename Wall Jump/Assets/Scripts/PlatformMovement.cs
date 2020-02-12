1d38019e9f04871d052b8237862508be58634b24
Werenskjold <Rilliden@gmail.com>
2020-02-12 20:20:07 +0900

    Started Third Person Camera Project

---
+++ Wall Jump/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    Transform startPosition;
+
+    void Start()
+    {
+        startPosition = gameObject.transform;
+    }
+
+    void Update()
+    {
+        Vector2 newPos = startPosition.position;
+        newPos.y = Mathf.Sin(Time.realtimeSinceStartup);
+        gameObject.transform.position = newPos;
+    }
+}
