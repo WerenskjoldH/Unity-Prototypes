0f3e755ef96ccaa6484b843e399e690364e7fa6d
Werenskjold <Rilliden@gmail.com>
2020-02-25 19:29:50 +0900

    Added Selection Effect Script (Mockup)

---
+++ Wall Jump/Assets/Scripts/MenuSelectScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuSelectScript : MonoBehaviour
+{
+    public TextMeshProUGUI uiText;
+    public bool selected = false;
+
+    private string startingText;
+    void Start()
+    {
+        startingText = uiText.text;
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bool intersect = gameObject.GetComponent<Collider2D>().bounds.Contains(mousePos);
+
+        if (!selected && intersect)
+        {
+            uiText.text = "-" + uiText.text + "-";
+            selected = true;
+        }
+        else if(selected && !intersect)
+        {
+            uiText.text = startingText;
+            selected = false;
+        }
+    }
+}
