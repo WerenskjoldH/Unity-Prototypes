faabe66116b186023a61c8503dcf5a28ac661cd8
Werenskjold <Rilliden@gmail.com>
2020-02-29 23:27:01 +0900

    Added Scene Transitions

---
+++ Wall Jump/Assets/Scripts/SceneTransitionerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionerScript : MonoBehaviour
+{
+    public Animator animator;
+    public float couroutineWaitTime = 1f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            LoadLevel("TestScene");
+    }
+
+    public void LoadLevel(string name)
+    {
+        StartCoroutine(LoadLevelCouroutine(name));
+    }
+
+    IEnumerator LoadLevelCouroutine(string name)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(couroutineWaitTime);
+
+        SceneManager.LoadScene(name);
+    }
+}
