8131e08c54b181daa93a0ac129b41e36bb493530
Werenskjold <Rilliden@gmail.com>
2020-03-14 00:36:00 +0900

    Fixed Step Swapping Coroutine Bug and Added Step 5

--- Wall Jump/Assets/Scripts/Tutorial/TutorialManagerScript.cs
+++ Wall Jump/Assets/Scripts/Tutorial/TutorialManagerScript.cs
@@ -20,18 +20,20 @@ public class TutorialManagerScript : MonoBehaviour
     {
         float timePassed = 0.0f;
 
+        // This prevents repeated calls
+        tutorialStep = -1;
+
         while(timePassed < waitTime)
         {
             timePassed += Time.deltaTime;
             yield return null;
         }
-
         tutorialStep = toWhichTutorialStep;
     }
 
     private void Start()
     {
-        textArea.text = "Welcome to the Tutorial\nPress <color=#d95763>Left Mouse Button</color> to begin";
+        textArea.text = "Welcome to the <color=#df7126>Tutorial</color>\nPress <color=#d95763>Left Mouse Button</color> to begin";
 
         playerScript.DisableGrinding();
         playerScript.DisableSticking();
@@ -47,10 +49,10 @@ public class TutorialManagerScript : MonoBehaviour
             {
                 textArea.text = "Fantastic!\nYou now know how to <color=#df7126>launch</color>";
                 playerFirstClick = true;
-                tutorialStep++;
+                tutorialStep = 1;
             }
         }else if(tutorialStep == 1) {
-            textArea.text = "Fantastic!\nYou have <color=#df7126>" + (playerScript.totalJumpsAllowed - playerScript.GetNumberJumpsUsed()) + "</color> jumps left";
+            textArea.text = "You now know how to <color=#df7126>launch</color>\nYou have <color=#df7126>" + (playerScript.totalJumpsAllowed - playerScript.GetNumberJumpsUsed()) + "</color> jumps left";
             if(playerScript.GetNumberJumpsUsed() == playerScript.totalJumpsAllowed)
             {
                 StartCoroutine(WaitToMoveToNextStep(2.0f, 2));
@@ -77,13 +79,34 @@ public class TutorialManagerScript : MonoBehaviour
             // Sticking To Walls
             // React differently based on the amount of jumps made in last step?
             textArea.text = "Now then, let's try <color=#df7126>sticking</color> to walls";
-            StartCoroutine(WaitToMoveToNextStep(5.0f, 4));
+            StartCoroutine(WaitToMoveToNextStep(4.0f, 4));
         }
         else if(tutorialStep == 4)
         {
             textArea.text = "To <color=#df7126>stick</color> to a wall hold the <color=#d95763>Right Mouse Button</color> before colliding with it\n" +
                 "I have highlighted the <color=#99e550>left wall</color> try to stick to it";
             leftWall.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(0.2517f, 0.6507f, 0.8980f);
+            GameObject stuckToObject = playerScript.GetObjectStuckTo();
+            if (stuckToObject == leftWall)
+            {
+                tutorialStep = 5;
+            }
+        }
+        else if(tutorialStep == 5)
+        {
+            textArea.text = "Now try to stick to the <color=#99e550>right wall</color>\n<color=#df7126>without</color> touching the ground";
+            leftWall.GetComponent<SpriteRenderer>().color = Color.white;
+            rightWall.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(0.2517f, 0.6507f, 0.8980f);
+            bottomWall.GetComponent<SpriteRenderer>().color = Color.HSVToRGB(0, 0.7093f, 0.6745f);
+        }
+        else if(tutorialStep == 6)
+        {
+            // Player touched the ground, tell them to go back to the left wall
+            // Point back to step 5
+        }
+        else if(tutorialStep == 7)
+        {
+            // Player made it to the right wall without touching the ground
         }
         
 
