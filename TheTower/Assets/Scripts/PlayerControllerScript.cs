427ef31a4a4d08979c74b1d3901088549481412e
Werenskjold <Rilliden@gmail.com>
2020-03-15 18:13:53 +0900

    Set Up Cursor

---
+++ TheTower/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject towerBase;
+    [SerializeField]
+    GameObject towerEye;
+    [SerializeField]
+    GameObject mouseGameObject;
+
+    Vector2 eyeDefaultPosition;
+
+    private void Start()
+    {
+        eyeDefaultPosition = towerEye.transform.position;
+    }
+
+    void EyeMovement()
+    {
+
+    }
+
+    private void Update()
+    {
+        EyeMovement();
+    }
+}
---
+++ TheTower/Assets/Scripts/PlayerMouseScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseScript : MonoBehaviour
+{
+    Vector2 mouseLocation;
+
+    public Vector2 GetMousePosition()
+    {
+        // Do we need to be so protective of the mouse location, I wonder?
+        return new Vector2(mouseLocation.x, mouseLocation.y);
+    }
+
+    private void Update()
+    {
+        mouseLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = mouseLocation;
+    }
+}
