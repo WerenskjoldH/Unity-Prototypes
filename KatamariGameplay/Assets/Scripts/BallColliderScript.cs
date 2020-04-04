8b4bcff6700061079a46f9244459f0c90d1309cc
Werenskjold <Rilliden@gmail.com>
2020-04-04 15:03:23 +0900

    Added Pickup Functionality

---
+++ KatamariGameplay/Assets/Scripts/BallColliderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColliderScript : MonoBehaviour
+{
+    [SerializeField]
+    BallControllerScript controllerScript;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Collectable")
+        {
+            collision.transform.parent = gameObject.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
