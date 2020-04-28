728ac2499209ab66ded984ffe177975a241dae2f
Werenskjold <Rilliden@gmail.com>
2020-04-28 23:23:58 +0900

    Updated Decepia To 2019.3.5f1

---
+++ Swap Shot/Assets/Scripts/Player/CameraSwap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Light playerLight;
+    public Camera monsterCamera;
+
+    void Start()
+    {
+        playerCamera.enabled = true;
+        playerCamera.GetComponent<AudioListener>().enabled = true;
+        monsterCamera.enabled = false;
+        monsterCamera.GetComponent<AudioListener>().enabled = false;
+        playerLight.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerCamera.enabled = false;
+            playerCamera.GetComponent<AudioListener>().enabled = false;
+            monsterCamera.enabled = true;
+            monsterCamera.GetComponent<AudioListener>().enabled = true;
+            playerLight.enabled = true;
+        }
+        
+        if(Input.GetMouseButtonUp(0))
+        {
+            playerCamera.enabled = true;
+            playerCamera.GetComponent<AudioListener>().enabled = true;
+            monsterCamera.enabled = false;
+            monsterCamera.GetComponent<AudioListener>().enabled = false;
+            playerLight.enabled = false;
+        }
+    }
+}
