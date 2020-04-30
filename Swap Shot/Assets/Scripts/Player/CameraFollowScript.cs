0706d9137f7fe981c760f4bcd94498c91dc24d97
Werenskjold <Rilliden@gmail.com>
2020-04-30 16:36:10 +0900

    Added "Update In Editor" Functionality To Player Camera Script

--- Swap Shot/Assets/Scripts/Player/CameraFollowScript.cs
+++ Swap Shot/Assets/Scripts/Player/CameraFollowScript.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 
+[ExecuteInEditMode]
 public class CameraFollowScript : MonoBehaviour
 {
     [SerializeField] GameObject TargetBody;
     [SerializeField] Vector3 bodyOffset;
 
+    [SerializeField] bool updateInEditor = true;
+
+    public void Awake()
+    {
+        if (Application.isPlaying)
+            updateInEditor = true;
+    }
+
     public void Update()
     {
-        gameObject.transform.position = TargetBody.transform.position + bodyOffset;
+        if (updateInEditor)
+            gameObject.transform.position = TargetBody.transform.position + bodyOffset;
+        else
+            gameObject.transform.position = TargetBody.transform.position;
+
     }
 }
