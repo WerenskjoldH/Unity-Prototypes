fd0412abab4b9f7bd4aeeb6f4882178e3b69cdb9
Werenskjold <Rilliden@gmail.com>
2020-05-06 13:31:15 +0900

    Created Spawn Point Object

---
+++ Swap Shot/Assets/Scripts/Level/Points/LevelStartPointScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStartPointScript : MonoBehaviour
+{
+    Transform playerTransform;
+    static bool multiplesFound = false;
+
+    void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    //Checks if the designer accidentally placed two spawn points
+    void DetectMultipleSpawnPoints()
+    {
+        if (multiplesFound)
+            return;
+
+        int numberOfSpawnPoints = GameObject.FindGameObjectsWithTag("StartPoint").Length;
+
+        if (numberOfSpawnPoints > 1)
+        {
+            Debug.LogError(numberOfSpawnPoints + " Start Points Found, Fix This Immediately");
+            multiplesFound = true;
+        }
+    }
+
+    void Start()
+    {
+        DetectMultipleSpawnPoints();
+
+        playerTransform.position = transform.position;
+    }
+}
