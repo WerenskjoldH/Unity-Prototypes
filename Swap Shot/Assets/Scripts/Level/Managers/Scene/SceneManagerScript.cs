6637acfaf3346c77c644cc8a1dd50e26debfd25b
Werenskjold <Rilliden@gmail.com>
2020-05-24 01:06:25 +0900

    Added Scene Transitions

---
+++ Swap Shot/Assets/Scripts/Level/Managers/Scene/SceneManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public Animator animator;
+    public float couroutineWaitTime = 1f;
+
+    public void LoadLevel(string name)
+    {
+        StartCoroutine(LoadLevelCouroutine(name));
+    }
+
+    IEnumerator LoadLevelCouroutine(string name)
+    {
+        animator.SetTrigger("Out");
+
+        yield return new WaitForSeconds(couroutineWaitTime);
+
+        SceneManager.LoadScene(name);
+    }
+}
