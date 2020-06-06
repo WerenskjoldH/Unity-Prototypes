c0b005930868822f2951fea5cce69f45e242dcd2
Werenskjold <Rilliden@gmail.com>
2020-06-06 14:30:00 +0900

    Kill Wall Opening is Now Script Driven

---
+++ Swap Shot/Assets/Shaders/Wall/KillWallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class KillWallScript : MonoBehaviour
+{
+    [SerializeField] MeshRenderer meshRenderer;
+    [SerializeField] Transform spherePoint;
+    [SerializeField] float radius;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        meshRenderer.sharedMaterial.SetFloat("Vector1_C5839ECE", radius);
+        meshRenderer.sharedMaterial.SetVector("Vector3_CE91E199", spherePoint.position);
+    }
+}
