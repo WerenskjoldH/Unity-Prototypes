730ef0b445fa0847aa43ad602a3f1a572b46644c
Werenskjold <Rilliden@gmail.com>
2020-08-10 16:34:45 +0900

    Added Weapon Selector

---
+++ Dysfunctional Defense/Assets/WeaponSelectorScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelectorScript : MonoBehaviour
+{
+    private bool isPaused = true;
+    Vector3 startingPosition;
+
+    [SerializeField]
+    float boundsDistance = 0;
+    [SerializeField]
+    float speedModifier = 1.0f;
+
+    [SerializeField]
+    int numberOfAbilities = 5;
+
+    float currentSelectionIndex = 0;
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+    }
+
+    public int GetCurrentAbilityIndex()
+    {
+        return (int)math.ceil(numberOfAbilities * (0.5f * (currentSelectionIndex + 1)));
+    }
+
+    void Start()
+    {
+        startingPosition = transform.position;
+
+        TogglePause();
+    }
+
+    void Update()
+    {
+        if (isPaused)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Debug.Log(GetCurrentAbilityIndex());
+
+        Vector3 position = transform.position;
+        currentSelectionIndex = Mathf.Sin(speedModifier * Time.time);
+        position.x = startingPosition.x + (boundsDistance * currentSelectionIndex);
+        transform.position = position;
+    }
+}
